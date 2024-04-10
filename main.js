b193d51bff75330cfb7cbb8cf946ef1bd1f83b2d
Beatriz Agante <beatrizagante@ua.pt>
2024-04-10 19:16:11 +0100

    style change

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import './styles.css';
+import './style.css';
 import * as THREE from 'https://threejs.org/build/three.module.js';
 import { OrbitControls } from 'https://threejs.org/examples/jsm/controls/OrbitControls.js';
 
